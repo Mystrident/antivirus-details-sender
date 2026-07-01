ce447376e49cfe4cddfc872fe69b50ccb12e171d
Mystrident <mystrident123@gmail.com>
2026-07-01 12:54:19 +0530

    trying to fix the transporter

--- backend/src/services/transporter.service.ts
+++ backend/src/services/transporter.service.ts
@@ -1,5 +1,8 @@
-// mail.ts
+import dns from "node:dns";
 import nodemailer from "nodemailer";
+
+// Prefer IPv4 over IPv6
+dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_SERVER,
@@ -13,5 +16,9 @@ export const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
+  },
+
+  tls: {
+    minVersion: "TLSv1.2",
   },
 });
